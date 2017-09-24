7a0e831b0549abafffd855db6f80963aeb1422f7
QuyTran <danangxp@gmail.com>
2017-09-24 22:10:11 +0700

    add new line New 2

--- src/main/java/quy/test/New2.java
+++ src/main/java/quy/test/New2.java
@@ -1,4 +1,10 @@
 package quy.test;
 
 public class New2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!11");
+        System.out.println("Hello World!dddd");
+        System.out.println("Hello World!33");
+
+    }
 }
