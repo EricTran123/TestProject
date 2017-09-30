71dcdd56db71abe4a360673faadc48c589be0573
QuyTran <danangxp@gmail.com>
2017-09-30 15:23:57 +0700

    add new line New 2

--- src/main/java/quy/test/App.java
+++ src/main/java/quy/test/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!11");
         System.out.println("Hello World!dddd");
+        System.out.println("Hello ");
         System.out.println("Hello World!33");
 
     }
